d8811c35028b2dc81780e8608e2b7cc47b449ae6
Ismael CasLop <mmcready07@gmail.com>
2020-06-22 21:54:23 -0500

    Se añada funcionalidad para envio de formularios y routes (no funcionan)

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -6,30 +6,31 @@ import { BrowserRouter, Link } from 'react-router-dom'
 import { Polices } from '../Polices/Polices'
 import { Claims } from '../Claims/Claims'
 
-export default class Head extends React.Component {
-    render() {
-        return (
-            <div>
-                <Navbar bg="dark" variant="dark" sticky="top">
-                    <Navbar.Brand>
-                        <img src={logo} alt="Devil May Cry" className='logo'/>
-                        <span className="head">Insurance Carrier</span>
-                    </Navbar.Brand>
-                    <Nav className="ml-auto">
-                      <BrowserRouter>
-                      <div>
-                        <Link to="/Polices">
-                          <Button variant="outline-primary" className='btn' >Polices</Button>
-                        </Link>
+function Head () {
 
-                        <Link to="/Claims">
-                          <Button variant="outline-success" className='btn' >Claims</Button>
-                        </Link>
-                      </div>
-                      </BrowserRouter>
-                    </Nav>
-                </Navbar>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <Navbar bg="dark" variant="dark" sticky="top">
+                <Navbar.Brand>
+                    <img src={logo} alt="Devil May Cry" className='logo'/>
+                    <span className="head">Insurance Carrier</span>
+                </Navbar.Brand>
+                <Nav className="ml-auto">
+                  <BrowserRouter>
+                    <div>
+                      <Link to="/Polices">
+                        <Button variant="outline-primary" className='btn' >Polices</Button>
+                      </Link>
+
+                      <Link to="/Claims">
+                        <Button variant="outline-success" className='btn' >Claims</Button>
+                      </Link>
+                    </div>
+                  </BrowserRouter>
+                </Nav>
+            </Navbar>
+        </div>
+    )
 }
+
+export default Head
